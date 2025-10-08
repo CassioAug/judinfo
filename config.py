0bfe01e4af0c22a104fab84ae4ae3240ef810f37
Cassio Soares <58272702+CassioAug@users.noreply.github.com>
2025-10-08 10:46:49 -0300

    update: config.py with api instructions

--- config.py
+++ config.py
@@ -1,1 +1,4 @@
+# API pública do DataJud conforme disponibiLizado em:
+# https://datajud-wiki.cnj.jus.br/api-publica/acesso
+# Em caso de problemas com a API, verificar se não foi alterada.
 API_KEY = "cDZHYzlZa0JadVREZDJCendQbXY6SkJlTzNjLV9TRENyQk1RdnFKZGRQdw=="
